32857249d9c41e9c5366e75349456a52834d60cd
wyf18340833480 <wyf18340833480@163.com>
2021-07-22 11:32:52 +0800

    delete ziduan

--- src/main/java/com/wyf/Stu.java
+++ src/main/java/com/wyf/Stu.java
@@ -1,10 +1,7 @@
 package com.wyf;
 
-import java.util.Date;
-
 public class Stu extends Person {
     private String name;
     private Integer age;
     private String addr;
-    private Date date;
 }
